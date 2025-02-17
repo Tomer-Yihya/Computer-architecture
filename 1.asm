4fd3222bb3490dc5c89d7b4677b744e6defd079d
TomK-TAU <tomkaninin@mail.tau.ac.il>
2025-02-18 00:22:43 +0200

    fix assembly programs

--- 1.asm
+++ 1.asm
@@ -1,14 +1,14 @@
-	add $r3, $zero, $imm, 127	# PC=0
+	add $r3, $zero, $imm, 127
 loop:
-	add $r4, $r4, $imm, 5	# PC=0
-	lw $r2, $zero, $imm, 0		# PC=1
-	add $r2, $r2, $imm, 1		# PC=2
-	sw $r2, $zero, $imm, 0		# PC=3
+	add $r4, $r4, $imm, 5
+	lw $r2, $zero, $imm, 0
+	add $r2, $r2, $imm, 1
+	sw $r2, $zero, $imm, 0
 wait:
-	blt $imm, $r6, $r4, wait	# PC=4
-	add $r6, $r6, $imm, 1		# PC=5
-	blt $imm, $r5, $r3, loop	# PC=4
-	add $r5, $r5, $imm, 1		# PC=5
+	blt $imm, $r6, $r4, wait
+	add $r6, $r6, $imm, 1
+	blt $imm, $r5, $r3, loop
+	add $r5, $r5, $imm, 1
 	lw $r2, $zero, $imm, 256	
 	halt $zero, $zero, $zero, 0	
 	halt $zero, $zero, $zero, 0	
